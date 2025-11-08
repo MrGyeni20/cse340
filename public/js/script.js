7cce6346479acd10adc86d97fe04f6285f2f4a53
MrGyeni20 <ngyeni@byupathway.edu>
2025-11-08 23:50:44 +0000

    week01

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// Highlight current page in navigation
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav a");
+  links.forEach(link => {
+    if (link.href === window.location.href) {
+      link.classList.add("active");
+    }
+  });
+});
